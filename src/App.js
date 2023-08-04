c27c3d75da0953f23a781e7ee3f55380e5b03446
Hemanth_S <hemanthkumar.s300@gmail.com>
2023-08-04 07:14:02 +0530

    Pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import PopChart from './components/PopChart.jsx';
-
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="container">  
-      <PopChart />
+      <Dashboard />
     </div>
   );
 }
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import PopChart from '../components/PopChart.jsx';
+
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='container'>
+        <PopChart />
+    </div>
+  )
+}
+
+export default Dashboard
